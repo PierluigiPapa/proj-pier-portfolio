4a250ea6c8b40b282d22da313f6221acc0328eb8
PierluigiPapa <pierluigipapa9@gmail.com>
2025-04-04 23:40:50 +0200

    fix: delete style border footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,9 +11,9 @@ export function Footer() {
                         LOGO PIERLUIGI PAPA
                     </span>
                 </a>
-                <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+                <ul className="flex flex-col font-medium p-4 md:p-0 mt-4  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                     <li>
-                        <a href="#" className="block py-2 px-3 text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-celeste transition-colors duration-300 ease-in-out transform md:p-0">Torna su</a>
+                        <a href="#" className="block py-2 px-3 text-white  md:hover:bg-transparent md:border-0 md:hover:text-celeste transition-colors duration-300 ease-in-out transform md:p-0">Torna su</a>
                     </li>
                 </ul>
             </div>
