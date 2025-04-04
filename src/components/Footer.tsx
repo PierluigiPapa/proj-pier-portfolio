302aa89a049690722a4d832add8d84e574dbe150
PierluigiPapa <pierluigipapa9@gmail.com>
2025-04-04 14:33:59 +0200

    fix: styled footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,14 +7,17 @@ export function Footer() {
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
             <div className="sm:flex sm:items-center sm:justify-between">
                 <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                    <span className="text-white hover:text-celeste transition-colors duration-300 ease-in-out transform">LOGO PIERLUIGI PAPA</span>
+                    <span className="text-white text-2xl hover:text-celeste transition-colors duration-300 ease-in-out transform">
+                        LOGO PIERLUIGI PAPA
+                    </span>
                 </a>
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                        <li>
-                            <a href="#" className="text-white hover:text-celeste transition-colors duration-300 ease-in-out transform">Torna su</a>
-                        </li>
-                    </ul>
+                <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+                    <li>
+                        <a href="#" className="block py-2 px-3 text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-celeste transition-colors duration-300 ease-in-out transform md:p-0">Torna su</a>
+                    </li>
+                </ul>
             </div>
+            
             <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
             <span className="block text-sm sm:text-center text-white hover:text-celeste transition-colors duration-300 ease-in-out transform">© 2025 <a href="" className="text-white hover:text-celeste transition-colors duration-300 ease-in-out transform">Pierluigi Papa</a>. Tutti i diritti riservati.</span>
         </div>
