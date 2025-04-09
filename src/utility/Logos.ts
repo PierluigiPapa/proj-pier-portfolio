dfa5e984b99edb45d64604c29a3c02d20ae6c4c9
PierluigiPapa <pierluigipapa9@gmail.com>
2025-04-09 13:15:12 +0200

    added logo's utility

---
+++ src/utility/Logos.ts
@@ -0,0 +1,16 @@
+export const logos = [
+    'AngularLogo.png',
+    'Bootstrap.png',
+    'CSS.png',
+    'Hmtl.png',
+    'JavaScript.png',
+    'Laravel.png',
+    'MySQL.png',
+    'PHP.png',
+    'PostgreSQL.png',
+    'SASS.png',
+    'SpringBoot.png',
+    'TypeScript.png',
+    'Vite.png',
+    'VueJS.png'
+]
