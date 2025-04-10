41fcf0f71982a9616253e69b3a012cd1f9754748
PierluigiPapa <pierluigipapa9@gmail.com>
2025-04-10 20:17:01 +0200

    styled FrameworkSection.tsx and body / added utility logos

--- src/components/FrameworkSection.tsx
+++ src/components/FrameworkSection.tsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from "react";
+import { logos } from "../utility/Logos";
+
 export function FrameworkSection() {
+    const [current, setCurrent] = useState(0);
+
+    const nextSlide = () => {
+        setCurrent((prev) => (prev + 1) % logos.length);
+    };
+
+    useEffect(() => {
+        const timer = setInterval(nextSlide, 3000);
+        return () => clearInterval(timer);
+    }, []);
+
+    const getIndex = (offset: number) => (current + offset + logos.length) % logos.length;
+
     return (
         <div>
             <h1 className="text-white text-5xl text-center font-bold my-10">Competenze tecniche</h1>
 
-            
+            <section className="w-full relative flex flex-col items-center">
+                <div className="flex justify-center items-center gap-20 h-32 md:h-48 overflow-hidden">
+                    {[getIndex(-1), current, getIndex(1)].map((index, i) => (
+                        <img
+                            key={index}
+                            src={logos[index]}
+                            alt={`logos ${index}`}
+                            className={`transition-all duration-100 ease-in-out
+                                ${i === 1 ? "scale-120 z-10 opacity-100" : "scale-70 opacity-50"}
+                                max-h-24 md:max-h-24`}
+                        />
+                    ))}
+                </div>
+            </section>
         </div>
     );
 }
---
+++ src/utility/Logos.ts
@@ -0,0 +1,32 @@
+import Angular from "../assets/images/logos/Angular.png";
+import Bootstrap from "../assets/images/logos/Bootstrap.png";
+import CSS from "../assets/images/logos/CSS.png";
+import HTML from "../assets/images/logos/Html.png";
+import JavaScript from "../assets/images/logos/JavaScript.png";
+import Laravel from "../assets/images/logos/Laravel.png";
+import MySQL from "../assets/images/logos/MySQL.png";
+import PHP from "../assets/images/logos/PHP.png";
+import PostgreSQL from "../assets/images/logos/PostgreSQL.png";
+import SASS from "../assets/images/logos/SASS.png";
+import SpringBoot from "../assets/images/logos/SpringBoot.png";
+import TypeScript from "../assets/images/logos/TypeScript.png";
+import Vite from "../assets/images/logos/Vite.png";
+import VueJS from "../assets/images/logos/VueJS.png";
+
+export const logos = [
+    Angular,
+    Bootstrap,
+    CSS,
+    HTML,
+    JavaScript,
+    Laravel,
+    MySQL,
+    PHP,
+    PostgreSQL,
+    SASS,
+    SpringBoot,
+    TypeScript,
+    Vite,
+    VueJS,
+
+];
