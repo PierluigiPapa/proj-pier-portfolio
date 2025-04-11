966a154d5123a0fc01165ad9ced037963aaa02d3
PierluigiPapa <pierluigipapa9@gmail.com>
2025-04-11 22:47:58 +0200

    feat: responsive framework section

--- src/components/FrameworkSection.tsx
+++ src/components/FrameworkSection.tsx
@@ -16,23 +16,23 @@ export function FrameworkSection() {
     const getIndex = (offset: number) => (current + offset + logos.length) % logos.length;
 
     return (
-        <div>
-            <h1 className="text-white text-5xl text-center font-bold my-10">Competenze tecniche</h1>
+        <>
+            <h1 className="text-white text-3xl whitespace-nowrap md:text-5xl text-center font-bold my-10">Competenze tecniche</h1>
 
-            <section className="w-full relative flex flex-col items-center">
-                <div className="flex justify-center items-center gap-20 h-32 md:h-48 overflow-hidden">
+            <section className="w-full relative flex flex-col items-center my-4">
+                <div className="flex justify-center items-center gap-5 md:gap-12 h-32 md:h-48 overflow-hidden">
                     {[getIndex(-1), current, getIndex(1)].map((index, i) => (
                         <img
                             key={index}
                             src={logos[index]}
                             alt={`logos ${index}`}
                             className={`transition-all duration-100 ease-in-out
-                                ${i === 1 ? "scale-120 z-10 opacity-100" : "scale-70 opacity-50"}
-                                max-h-24 md:max-h-24`}
+                                ${i === 1 ? "scale-110 z-10 opacity-100" : "scale-60 opacity-50"}
+                                max-h-20 md:max-h-24`}
                         />
                     ))}
                 </div>
             </section>
-        </div>
+        </>
     );
 }
