f7595258841bc715ff1471fcc3e7c010f092b973
PierluigiPapa <pierluigipapa9@gmail.com>
2025-04-12 21:11:26 +0200

    fix: added new logos / update logo

--- src/utility/Logos.ts
+++ src/utility/Logos.ts
@@ -1,31 +1,61 @@
 import Angular from "../assets/images/logos/Angular.png";
+import Axios from "../assets/images/logos/Axios.png";
 import Bootstrap from "../assets/images/logos/Bootstrap.png";
+import CSharp from "../assets/images/logos/CSharp.png";
 import CSS from "../assets/images/logos/CSS.png";
+import Docker from "../assets/images/logos/Docker.webp";
+import Git from "../assets/images/logos/Git.png";
 import HTML from "../assets/images/logos/Html.png";
+import Java from "../assets/images/logos/Java.png";
 import JavaScript from "../assets/images/logos/JavaScript.png";
 import Laravel from "../assets/images/logos/Laravel.png";
+import MAMP from "../assets/images/logos/MAMP.png";
+import MongoDB from "../assets/images/logos/MongoDB.png";
 import MySQL from "../assets/images/logos/MySQL.png";
+import NET from "../assets/images/logos/NET.png";
+import NodeJS from "../assets/images/logos/NodeJS.png";
 import PHP from "../assets/images/logos/PHP.png";
+import PHPMyAdmin from "../assets/images/logos/PhpMyAdmin.png";
 import PostgreSQL from "../assets/images/logos/PostgreSQL.png";
+import ReactJS from "../assets/images/logos/React.png";
 import SASS from "../assets/images/logos/SASS.png";
+import SMSS from "../assets/images/logos/SSMS.png";
 import SpringBoot from "../assets/images/logos/SpringBoot.png";
+import Tailwind from "../assets/images/logos/Tailwind.png"
 import TypeScript from "../assets/images/logos/TypeScript.png";
+import VisualStudio from "../assets/images/logos/Visual Studio.png";
+import VisualStudioCode from "../assets/images/logos/Visual Studio Code.png";
 import Vite from "../assets/images/logos/Vite.png";
 import VueJS from "../assets/images/logos/VueJS.png";
 
 export const logos = [
     Angular,
+    Axios,
     Bootstrap,
+    CSharp,
     CSS,
+    Docker,
+    Git,
     HTML,
+    Java,
     JavaScript,
     Laravel,
+    MAMP,
+    MongoDB,
     MySQL,
+    NET,
+    NodeJS,
     PHP,
+    PHPMyAdmin,
     PostgreSQL,
+    ReactJS,
     SASS,
+    SMSS,
     SpringBoot,
+    Tailwind,
     TypeScript,
+    VisualStudio,
+    VisualStudioCode,
     Vite,
     VueJS,
 
