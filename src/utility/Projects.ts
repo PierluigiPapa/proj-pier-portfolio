eb72d97769b13d97be03d2020421adb1b05e2e2b
PierluigiPapa <pierluigipapa9@gmail.com>
2025-05-05 13:33:57 +0200

    added projects utility

---
+++ src/utility/Projects.ts
@@ -0,0 +1,11 @@
+import BoolBnb from "../assets/images/projects/BoolBnB.png";
+ import CardPokemon from "../assets/images/projects/Card_Pokemon.png";
+ import MasterStudy from "../assets/images/projects/MasterStudy.jpg";
+ import YuGiOh from "../assets/images/projects/Yu_Gi_Oh.png";
+ 
+ export const project = [
+     BoolBnb,
+     CardPokemon,
+     MasterStudy,
+     YuGiOh
+ ]
