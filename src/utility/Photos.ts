05777e87f7bbf9b11f5adf5a5cc0bd4667e391f2
PierluigiPapa <pierluigipapa9@gmail.com>
2025-08-16 12:34:03 +0200

    fixed photos.ts

--- src/utility/Photos.ts
+++ src/utility/Photos.ts
@@ -2,11 +2,8 @@ import Pierluigi1 from "../assets/images/photos/Pierluigi_2.webp"
 import Pierluigi2 from "../assets/images/photos/Pierluigi_4.webp"
 import Pierluigi3 from "../assets/images/photos/Pierluigi_3.webp"
 
-import Boolean from "../assets/images/photos/Attestato Boolean.png"
-
 export const photos = [
     Pierluigi1,
     Pierluigi2,
     Pierluigi3,
-    Boolean,
 ]
