a05d13df20c87a5a146b4089c6a4e58ab0c5e082
Pierluigi Papa <pierluigipapa9@gmail.com>
2026-01-18 21:20:56 +0100

    fix aggiunta loghi e nuovo CV

--- src/utility/Logos.ts
+++ src/utility/Logos.ts
@@ -1,9 +1,11 @@
 import Angular from '../assets/images/logos/Angular.webp';
 import Axios from '../assets/images/logos/Axios.webp';
 import Bootstrap from '../assets/images/logos/Bootstrap.webp';
+import ChatGPT from '../assets/images/logos/ChatGPT.webp';
 import CSharp from '../assets/images/logos/CSharp.webp';
 import CSS from '../assets/images/logos/CSS.webp';
 import Docker from '../assets/images/logos/Docker.webp';
+import Figma from '../assets/images/logos/Figma.webp';
 import Git from '../assets/images/logos/Git.webp';
 import HTML from '../assets/images/logos/Html.webp';
 import Java from '../assets/images/logos/Java.webp';
@@ -13,10 +15,12 @@ import MAMP from '../assets/images/logos/MAMP.webp';
 import MongoDB from '../assets/images/logos/MongoDB.webp';
 import MySQL from '../assets/images/logos/MySQL.webp';
 import NET from '../assets/images/logos/NET.webp';
+import Nest from '../assets/images/logos/Nest.webp';
 import NextJS from '../assets/images/logos/NextJS.webp';
 import NodeJS from '../assets/images/logos/NodeJS.webp';
 import PHP from '../assets/images/logos/PHP.webp';
 import PHPMyAdmin from '../assets/images/logos/PhpMyAdmin.webp';
+import Prisma from '../assets/images/logos/Prisma.webp';
 import PostgreSQL from '../assets/images/logos/PostgreSQL.webp';
 import ReactJS from '../assets/images/logos/React.webp';
 import SASS from '../assets/images/logos/SASS.webp';
@@ -28,14 +32,17 @@ import VisualStudio from '../assets/images/logos/Visual Studio.webp';
 import VisualStudioCode from '../assets/images/logos/Visual Studio Code.webp';
 import Vite from '../assets/images/logos/Vite.webp';
 import VueJS from '../assets/images/logos/VueJS.webp';
+import ZenStack from '../assets/images/logos/ZenStack.webp';
 
 export const logos = [
   Angular,
   Axios,
   Bootstrap,
+  ChatGPT,
   CSharp,
   CSS,
   Docker,
+  Figma,
   Git,
   HTML,
   Java,
@@ -44,12 +51,14 @@ export const logos = [
   MAMP,
   MongoDB,
   MySQL,
+  Nest,
   NET,
   NextJS,
   NodeJS,
   PHP,
   PHPMyAdmin,
   PostgreSQL,
+  Prisma,
   ReactJS,
   SASS,
   SMSS,
@@ -60,4 +69,5 @@ export const logos = [
   VisualStudioCode,
   Vite,
   VueJS,
+  ZenStack,
 ];
